d3f6a52781ca6308cd39d66c6f41692ae227a1a4
envyseapets <envyseapets@2f43e883-caf6-67b7-7e67-f8f7d4f62005>
2010-12-07 01:28:00 +0000

    - Fixed author - No need for license text in README if we are including LICENSE file

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name="rhythmbox-ampache",
     version="0.11",
 	description="A Rhythmbox plugin to stream music from an Ampache server",
-	author="Seva Epsteyn",
-	author_email="seva@sevatech.com",
+	author="Rhythmbox Ampache plugin team",
+	author_email="rhythmbox-ampache@googlegroups.com",
 	url="http://code.google.com/p/rhythmbox-ampache",
 	packages= ["ampache"],
 	)
